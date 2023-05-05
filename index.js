aeb3d2d4024ee4eedd308992deb52a8edd5e6450
Eric Edouard <eamilhat@gmail.com>
2023-05-05 11:12:24 +0200

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+    configs: {
+        node: {},
+        react: {
+            plugins: ['myPlugin'],
+            env: ['node'],
+            rules: {
+                'myPlugin/my-rule': 'off',
+                'eslint-plugin-myPlugin/another-rule': 'off',
+                'eslint-plugin-myPlugin/yet-another-rule': 'error',
+            },
+        },
+        'react-native': {
+            plugins: ['myPlugin'],
+            env: ['browser'],
+            rules: {
+                semi: 'error',
+                'myPlugin/my-rule': 'error',
+                'eslint-plugin-myPlugin/another-rule': 'error',
+            },
+        },
+    },
+};
