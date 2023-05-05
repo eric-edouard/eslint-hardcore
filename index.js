d9bea2f51d0addd58f56a982b08b0616e2eacd84
Eric Edouard <eamilhat@gmail.com>
2023-05-05 14:30:19 +0200

    configs

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 module.exports = {
     configs: {
-        node: {},
+        recommended: {
+            plugins: [
+                'json',
+                'import',
+                'promise',
+                'sonarjs',
+                'prettier',
+                'filenames',
+            ],
+            extends: [
+                'eslint:recommended',
+                'plugin:import/recommended',
+                'plugin:import/typescript',
+                'plugin:promise/recommended',
+                'plugin:json/recommended-with-comments',
+                'plugin:@typescript-eslint/recommended',
+                'plugin:@typescript-eslint/recommended-requiring-type-checking',
+                'plugin:prettier/recommended',
+            ],
+            rules: [
+
+            ]
+        },
         react: {
-            plugins: ['myPlugin'],
-            env: ['node'],
-            rules: {
-                'myPlugin/my-rule': 'off',
-                'eslint-plugin-myPlugin/another-rule': 'off',
-                'eslint-plugin-myPlugin/yet-another-rule': 'error',
-            },
+            plugins: ['hooks'],
+            extends: [
+                'plugin:react/recommended'
+            ],
         },
         'react-native': {
-            plugins: ['myPlugin'],
-            env: ['browser'],
-            rules: {
-                semi: 'error',
-                'myPlugin/my-rule': 'error',
-                'eslint-plugin-myPlugin/another-rule': 'error',
+            env: {
+                'react-native/react-native': true
             },
+            plugins: ['hooks', 'react-native'],
+            extends: ['@react-native-community', 'plugin:react/recommended'],
         },
     },
 };
